311437a061330211d2b37b40bb37fbdf9674fbec
Jayanta Samaddar <jay.s@kollablifestyle.com>
2022-05-07 17:14:19 +0530

    useReducer hook implementation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
+import { useReducer, useRef } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import './App.css';
 
-function App() {
+const taskReducers = (state, { type, payload }) => {
+  switch (type) {
+    case 'onChange':
+      return {
+        ...state,
+        currentTask: { ...state.currentTask, title: payload },
+      };
+
+    case 'addTask':
+      return {
+        ...state,
+        tasks: [
+          ...state.tasks,
+          { ...state.currentTask, id: uuidv4(), status: 'open' },
+        ],
+        totalTasks: state.totalTasks + 1,
+        currentTask: { title: '' },
+      };
+    case 'deleteTask':
+      return {
+        ...state,
+        tasks: state.tasks.filter(task => task.id !== payload),
+        totalTasks: state.totalTasks - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+const initialState = {
+  tasks: [],
+  totalTasks: 0,
+  currentTask: {},
+};
+
+export default function App() {
+  const taskRef = useRef();
+  const [tasks, dispatch] = useReducer(taskReducers, initialState);
+
+  console.log({ tasks });
+
+  // const addTask = () => dispatch({ type: 'onClick' })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1 className="AppTitle">Task App</h1>
+      <div className="inputArea">
+        <input
+          type="text"
+          name="task"
+          value={tasks.currentTask.title}
+          placeholder="Add a Task"
+          onChange={e =>
+            dispatch({ type: 'onChange', payload: e.target.value })
+          }
+        />
+        <button
+          className="CTABtn"
+          onClick={() => dispatch({ type: 'addTask' })}
         >
-          Learn React
-        </a>
-      </header>
+          Add Task
+        </button>
+      </div>
+      {tasks.tasks.length > 0 && (
+        <div className="tasksArea">
+          <h3>List of Tasks</h3>
+          {tasks.tasks.map((task, indx) => {
+            return (
+              <div
+                className="task"
+                key={task.id}
+                data-id={task.id}
+                ref={taskRef}
+              >
+                <div className="taskTitle">{task.title}</div>
+                <div className="taskActions">
+                  <button classname="Edit">Edit</button>
+                  <button
+                    classname="Delete"
+                    onClick={() =>
+                      dispatch({ type: 'deleteTask', payload: task.id })
+                    }
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
     </div>
   );
 }
-
-export default App;
